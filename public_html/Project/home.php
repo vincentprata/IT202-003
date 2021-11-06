e91fd3f4be4ef29cc8af007c4c51de4c8fa901ed
vincentprata <vincentprata41@gmail.com>
2021-11-06 16:17:22 -0400

    updated changes

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -3,6 +3,7 @@ require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
+
 if (is_logged_in(true)) {
     echo "Welcome home, " . get_username();
     //comment this out if you don't want to see the session variables
