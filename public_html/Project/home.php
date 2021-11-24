737d2717d37f3e339ba92a6cede8da946f310087
vincentprata <vincentprata41@gmail.com>
2021-11-24 11:19:08 -0500

    updated changes

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,14 +1,15 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<h1>Home</h1>
-<?php
-if (is_logged_in(true)) {
-    echo "Welcome home, " . get_username();
-    //comment this out if you don't want to see the session variables
-    echo "<pre>" . var_export($_SESSION, true) . "</pre>";
-}
-?>
-<?php
-require(__DIR__ . "/../../partials/flash.php");
+<div class="container-fluid">
+    <h1>Home</h1>
+    <?php
+    /*if (is_logged_in(true)) {
+    echo "Welcome, " . get_username();
+} else {
+    echo "You're not logged in";
+}*/
+    //shows session info
+    //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+require(__DIR__ . "/../../partials/footer.php");
 ?>
